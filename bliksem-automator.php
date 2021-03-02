c890693da90da03aaf5a0650dd1da1cc81cdb8ac
andrenellin <andrenellin@gmail.com>
2021-03-01 23:17:19 -0700

    Organized plugin folders

---
+++ bliksem-automator.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Bliksem Automator
+Plugin URI: https://github.com/andrenellin/bliksem-automator
+Description: Adds functionality to Automator plugin
+Author: Andre Nell
+Version: 1.0
+Author URI: https://www.andrenell.me
+*/
+
+/*
+ * Includes
+ * =========
+ *
+ */
+
+
+include_once dirname( __FILE__ ) . '/includes/mp-recurringsubscriptionpaused.php';
