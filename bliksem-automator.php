26a975046e4707e94f922e761356bd872ee85d1c
andrenellin <28567813+andrenellin@users.noreply.github.com>
2021-07-27 13:19:55 -0600

    Update bliksem-automator.php

--- bliksem-automator.php
+++ bliksem-automator.php
@@ -22,12 +22,12 @@
 include_once dirname(__FILE__) . '/includes/mp-recurringsubscriptionpaused.php';
 include_once dirname(__FILE__) . '/includes/mp-recurringsubscriptionresumed.php';
 
-function bliksem_custom_logs($message)
-{
-    if (is_array($message)) {
-        $message = json_encode($message);
-    }
-    $file = fopen("../custom_logs.log", "a");
-    echo fwrite($file, "\n" . date('Y-m-d h:i:s') . " :: " . $message);
-    fclose($file);
-}
+// function bliksem_custom_logs($message)
+// {
+//     if (is_array($message)) {
+//         $message = json_encode($message);
+//     }
+//     $file = fopen("../custom_logs.log", "a");
+//     echo fwrite($file, "\n" . date('Y-m-d h:i:s') . " :: " . $message);
+//     fclose($file);
+// }
